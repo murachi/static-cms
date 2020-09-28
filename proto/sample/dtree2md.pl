d65b4c2c2ba63635da07020fc442b4376c7918eb
Murayama Toshiyuki (T.MURACHI) <harapekomurachi@gmail.com>
2020-09-29 01:31:26 +0900

    issue #2 - テスト用データ生成スクリプトを作成。

--- proto/sample/dtree2md.pl
+++ proto/sample/dtree2md.pl
@@ -1,12 +1,11 @@
 use strict;
-use utf8;
-use List::Util;
+use List::Util qw/any/;
 
 my %ext2fence = (
   cpp => sub { $_[0]->{'c++'} = 1; 'c++' },
   h => sub { exists $_[0]->{'c++'} ? 'c++' :'c' },
   csproj => sub { 'xml' },
-  cs => sub { 'c#' }
+  cs => sub { 'c#' },
   stetic => sub { 'xml' },
   pl => sub { 'perl' },
   py => sub { 'python' },
@@ -55,17 +54,41 @@ my @ignore_pattern;
 sub walk {
   my ($cd, $depth) = @_;
   opendir my $dh, $cd or die "ディレクトリ $cd を開けません";
-  print "#" x ($depth + 1), " `$cd`\n\n";
+  print "#" x $depth, " `$cd`\n\n";
+  my $dir_stat = {};
   my @items = grep { !/^\.+$/ } readdir $dh;
+  # リポジトリルート下の README.md は最初にそのまま出力する
+  if ($depth == 1) {
+    @items = grep {
+      if ($_ =~ /^readme\.md$/i) {
+        open my $fin, "<$cd/$_" or die "ファイル $cd/$_ を開けません";
+        while (<$fin>) { chomp; print "$_\n"; }
+        print "\n-----\n\n";
+        0
+      }
+      else { 1 }
+    } @items;
+  }
+  # .gitignore があれば情報を収集
+  if (any { $_ eq '.gitignore' } @items) {
+    open my $fin, "<$cd/.gitignore" or die "ファイル $cd/.gitignore を開けません";
+    my @patterns = <$fin>;
+    chomp @patterns;
+    push @ignore_pattern, { base => $cd, patterns => [@patterns] };
+  }
+
   ITEMS_LOOP:
   for my $file (@items) {
     my $item = "$cd/$file";
-    # .ignore 適用対象を除外
-    for my ($base, $patterns) (map { @$_{qw(base patterns)} } @ignore_pattern) {
+    # .gitignore 適用対象を除外
+    for my $igpat (@ignore_pattern) {
+      my ($base, $patterns) = @$igpat{qw(base patterns)};
       my $is_skip = 0;
-      for my $pat (@$patterns) {
-        my $hit_flag = !$pat =~ /^\~/;
+      for my $pattern (@$patterns) {
+        my $pat = "$pattern";
+        my $hit_flag = !($pat =~ /^\~/);
         $pat = $hit_flag ? $pat : substr $pat, 1;
+        $pat =~ s/\./\\./g;
         $pat =~ s/\*/.*?/g;
         $pat =~ s!/$!!;
         if ($pat =~ m!^/!) {
@@ -77,19 +100,41 @@ sub walk {
       }
       next ITEMS_LOOP if $is_skip;
     }
+    # ディレクトリなら再帰
     walk($item, $depth + 1), next if -d $item;
-
-    my ($ext) = $file =~ /\.(.*?)$/ || '';
+    # fence 情報を決める
+    my ($ext) = $file =~ /\.(.*?)$/ or ('');
     next if exists $ng_ext{$ext};
-
-    open my $fin, "<$item" or die "ファイル $item を開けません";
-    print "- $file\n  ```";
     my $fence = exists $fn2fence{$file} ? $fn2fence{$file}->() : '';
+    unless ($fence) {
+      for my $p2f (@pat2fence) {
+        my ($pat, $fc) = @$p2f{qw(pat fence)};
+        $fence = $fc, last if $file =~ /$pat/;
+      }
+    }
+    $fence = $fence || (exists $ext2fence{$ext} ? $ext2fence{$ext}->($dir_stat) : '');
+    # Markdown として出力
+    open my $fin, "<$item" or die "ファイル $item を開けません";
+    print "- $file\n  ```$fence\n";
+    while (<$fin>) {
+      chomp;
+      print "  $_\n";
+    }
+    print "  ```\n";
   }
 }
 
-print STDERR ("収集するソースツリーがあるディレクトリを指定してください。"), exit 1
-  unless @ARGV;
+unless (@ARGV) {
+  print STDERR ("収集するソースツリーがあるディレクトリを指定してください。\n");
+  exit 1;
+}
 my $root = $ARGV[0];
-print STDERR ("存在するディレクトリを指定してください。"), exit 1
-  unless -d $root;
+$root =~ s!/$!!;
+
+unless (-d $root) {
+  print STDERR ("存在するディレクトリを指定してください。\n");
+  exit 1;
+}
+
+push @ignore_pattern, { base => $root, patterns => ['.git/'] };
+walk($root, 1);
