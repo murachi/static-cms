57e98250f82edc46accaa57722a25a6cec01ffb6
Murayama Toshiyuki <harapekomurachi@gmail.com>
2020-11-04 15:06:58 +0900

    issue #2 - Rust で GNU Source-highlight を用いる環境を準備中…。

--- proto/rust/cm2html-gnu-sh/src/c/srchilite-wrapper.cpp
+++ proto/rust/cm2html-gnu-sh/src/c/srchilite-wrapper.cpp
@@ -3,19 +3,27 @@
 #include <string>
 #include <srchilite/sourcehighlight.h>
 #include <srchilite/langmap.h>
+#include <cstring>
 
 using namespace std::string_literals;
 
 extern "C" {
   typedef struct {
-    srchilite::SourceHighlight impl
+    srchilite::SourceHighlight src_highlight,
+    srchilite::LangMap lang_map,
+    std::string lang_file,
+    std::string result,
   } SourceHighlightImpl;
 
   SourceHighlightImpl* SourceHighlight_new(char const* output_lang, char const* data_dir)
   {
     std::string ol = output_lang && output_lang[0] ? output_lang : "html.outlang";
-    auto shi = new SourceHighlightImpl { srchilite::SourceHighlight{ol} };
-    shi->impl.setDataDir(data_dir && data_dir[0] ? data_dir : "");
+    std::string dd = data_dir && data_dir[0] ? data_dir : "";
+    auto shi = new SourceHighlightImpl {
+      srchilite::SourceHighlight{ol},
+      srchilite::LangMap{dd, "lang.map"s},
+    };
+    shi->src_highlight.setDataDir(dd);
     return shi;
   }
 
@@ -24,5 +32,21 @@ extern "C" {
     delete shi;
   }
 
+  bool SourceHighlight_setLanguage(SourceHighlightImpl * shi, char const* language)
+  {
+    shi->lang_file = shi->lang_map.getMappedFileName(language);
+    return !shi->lang_file.empty();
+  }
 
+  char const* SourceHighlight_highlight(SourceHighlightImpl * shi, char const* src)
+  {
+    if (shi->lang_file.empty()) {
+      return nullptr;
+    }
+    std::istringstream istrm{src};
+    std::ostringstream ostrm;
+    shi->src_highlight.highlight(istrm, ostrm, shi->lang_file);
+    shi->result = ostrm.str();
+    return shi->result.c_str();
+  }
 }
